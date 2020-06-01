eea7b5cec94a7b19b97d5d034a37740645cb5d72
Davide Cerato <mrc3drik@gmail.com>
2020-06-01 10:46:40 +0200

    scraped relevant datas of best films

--- imdb/spiders/best_movies.py
+++ imdb/spiders/best_movies.py
@@ -7,15 +7,20 @@ from scrapy.spiders import CrawlSpider, Rule
 class BestMoviesSpider(CrawlSpider):
     name = 'best_movies'
     allowed_domains = ['www.imdb.com']
-    start_urls = ['http://www.imdb.com/']
+    start_urls = ['https://www.imdb.com/chart/top/?ref_=nv_mv_250']
 
     rules = (
-        Rule(LinkExtractor(allow=r'Items/'), callback='parse_item', follow=True),
+        Rule(LinkExtractor(
+            restrict_xpaths='//td[@class="titleColumn"]/a'), callback='parse_item', follow=True),
     )
 
     def parse_item(self, response):
-        item = {}
-        #item['domain_id'] = response.xpath('//input[@id="sid"]/@value').get()
-        #item['name'] = response.xpath('//div[@id="name"]').get()
-        #item['description'] = response.xpath('//div[@id="description"]').get()
-        return item
+        yield{
+            'title': response.xpath('//div[@class="title_wrapper"]/h1/text()').get(),
+            'year': response.xpath('//span[@id="titleYear"]/a/text()').get(),
+            'rating': response.xpath('//span[@itemprop = "ratingValue"]/text()').get(),
+            'duration': response.xpath('(//time)[1]/text()').get().replace("\n", "").replace(" ", "").replace("h", "h "),
+            'genre': response.xpath('//div[@class="subtext"]/a[1]/text()').get(),
+            'movie url': response.url,
+            'trailer url': response.urljoin(response.xpath('//a[@class="slate_button prevent-ad-overlay video-modal"]/@href').get())
+        }
